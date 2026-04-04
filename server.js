b38d8fd52e1e68f0a0b63d68cfa7d8dbf92c441c
Setthawuth-dev <setthawuth.dev@gmail.com>
2026-04-04 15:13:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/api/ask-ai', async (req, res) => {
         console.log("กำลังถาม Gemini...");
         
         // 🚀 ลองเปลี่ยนมาใช้รุ่นล่าสุดดูครับ (ถ้ายัง Error เราจะเอาชื่อจากใน Log ข้างบนมาใส่แทน)
-        const model = genAI.getGenerativeModel({ model: "gemini-3.1-flash-lite-preview" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
         
         const result = await model.generateContent(prompt);
         const responseText = result.response.text();
